c664d101544244a2156a9691029ec01992ecca5a
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-01-03 11:50:26 +0200

    Revisit offline middleware

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ export const RESULT_FILE_PATH = readEnvironmentVariable('RESULT_FILE_PATH');
 // Connection
 export const API_KEYS = readEnvironmentVariable('API_KEYS');
 export const HTTP_PORT = readEnvironmentVariable('HTTP_PORT', {defaultValue: 8080, format: v => Number(v)});
-export const OFFLINE_PERIOD = readEnvironmentVariable('OFFLINE_PERIOD', {defaultValue: '0,0'});
+export const [OFFLINE_BEGIN, OFFLINE_DURATION] = readEnvironmentVariable('OFFLINE_PERIOD', {defaultValue: '0,0'}).split(',');
 
 // General
 export const MANDATORY_PARAMETERS = [
--- src/services/middlewareService.js
+++ src/services/middlewareService.js
@@ -1,25 +1,25 @@
 import HttpStatus from 'http-status';
-import {API_KEYS, OFFLINE_PERIOD} from '../config';
+import {API_KEYS, OFFLINE_BEGIN, OFFLINE_DURATION} from '../config';
 import moment from 'moment';
 
 import {Utils} from '@natlibfi/melinda-commons';
 const {createLogger} = Utils;
 const logger = createLogger(); // eslint-disable-line no-unused-vars
 
+// Is this needed? Already implemented in Rest-api-importer
 export function createOfflineHoursMiddleware() {
 	return (req, res, next) => {
-		const [begin, duration] = OFFLINE_PERIOD.split(',');
-		logger.log('info', `Offline hours begin at ${begin} and will last next ${duration} hours. Time is now ${moment().format('HH:mm')}`);
+		logger.log('info', `Offline hours begin at ${OFFLINE_BEGIN} and will last next ${OFFLINE_DURATION} hours. Time is now ${moment().format('HH:mm')}`);
 		const now = moment();
-		const start = moment(now).startOf('day').add(begin, 'hours');
-		const end = moment(start).add(duration, 'hours');
-		if (now.hours() < begin && start.format('DDD') < end.format('DDD')) { // Offline hours pass midnight (DDD = day of the year)
+		const start = moment(now).startOf('day').add(OFFLINE_BEGIN, 'hours');
+		const end = moment(start).add(OFFLINE_DURATION, 'hours');
+		if (now.hours() < OFFLINE_BEGIN && start.format('DDD') < end.format('DDD')) { // Offline hours pass midnight (DDD = day of the year)
 			start.subtract(1, 'days');
 			end.subtract(1, 'days');
 		}
 
 		if (now.format('x') >= start.format('x') && now.format('x') < end.format('x')) {
-			res.status(HttpStatus.SERVICE_UNAVAILABLE).send(`${HttpStatus['503_MESSAGE']} Offline hours begin at ${begin} and will last next ${duration} hours.`).end();
+			res.status(HttpStatus.SERVICE_UNAVAILABLE).send(`${HttpStatus['503_MESSAGE']} Offline hours begin at ${OFFLINE_BEGIN} and will last next ${OFFLINE_DURATION} hours.`).end();
 		} else {
 			return next();
 		}
